c6c18f765797e7696718e423b6843e739d8887b1
Manan Upadhyay <manantheassassin@gmail.com>
2021-08-12 15:32:49 +0530

    Register and Login Backend Implemented

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route   GET api/auth
 // @desc    Get loggedin user
@@ -13,8 +19,55 @@ router.get("/", (req, res) => {
 // @desc    Auth user and get token
 // @access  Public
 
-router.post("/", (req, res) => {
-  res.send("Login User");
-});
+router.post(
+  "/",
+  [
+    body("email", "Please include a valid email").isEmail(),
+    body("password", "Password is Required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Email not registered" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Password" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactKeeper API..." })
 );
 
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
